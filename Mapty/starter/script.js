4da1706dcfb1972cbc7de630de68690cb22b57df
Isaac Ayomide <okunlolaa3@outlook.com>
2024-07-09 21:44:17 +0100

    revising navigator.geolocation

--- Mapty/starter/script.js
+++ Mapty/starter/script.js
@@ -12,16 +12,27 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 // Using the geolocation API (sucess fn, error fn)
-if(navigator.geolocation)
-navigator.geolocation.getCurrentPosition(function(position){
-  console.log(position)
-  // Getting the longitude and latitude
-  const {longitude} = position.coords;
-  const {latitude} = position.coords;
-  console.log(latitude, longitude);
-  
-  // Getting your current location on google map
-  console.log(`https://www.google.pt/maps/@${latitude}, ${longitude}`)
-}, function(){
-  alert('Could not get current position')
-})
+if (navigator.geolocation)
+  // Takes two callback functions (success and the error callback)
+  navigator.geolocation.getCurrentPosition(
+    // Success callback
+    function (position) {
+      console.log(position);
+      // Getting the longitude and latitude
+      // const latitude = position.coords.latitude
+      // const longitude = position.coords.longitude
+      const { longitude } = position.coords;
+      const { latitude } = position.coords;
+      console.log(latitude, longitude);
+
+      // Getting your current location on google map
+      console.log(`https://www.google.pt/maps/@${latitude}, ${longitude}`);
+    },
+
+    // Error callback
+    function () {
+      alert('Could not get current position');
+    }
+  );
+
+console.log(navigator.geolocation);
