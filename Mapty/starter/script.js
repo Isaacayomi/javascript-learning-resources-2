da25d7fdc701a8c8b4ea6214b63a04059cb90b29
Isaac Ayomide <okunlolaa3@outlook.com>
2024-07-13 21:34:03 +0100

    refactoring for project architecture

--- Mapty/starter/script.js
+++ Mapty/starter/script.js
@@ -11,79 +11,98 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 let map, mapEvent;
-// Using the geolocation API (sucess fn, error fn)
-if (navigator.geolocation)
-  // Takes two callback functions (success and the error callback)
-  navigator.geolocation.getCurrentPosition(
+
+class App {
+  #map;
+  #mapEvent;
+  constructor() {
+    this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+
+    // Toggles the form 'select' type
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
+
+  _getPosition() {
+    if (navigator.geolocation)
+      // Takes two callback functions (success and the error callback)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        // Error callback
+        function () {
+          alert('Could not get current position');
+        }
+      );
+  }
+
+  _loadMap(position) {
     // Success callback
-    function (position) {
-      console.log(position);
-      // Getting the longitude and latitude
-      // const latitude = position.coords.latitude
-      // const longitude = position.coords.longitude
-      const { longitude } = position.coords;
-      const { latitude } = position.coords;
-      console.log(latitude, longitude);
-      // Getting your current location on google map
-      // console.log(`https://www.google.pt/maps/@${latitude}, ${longitude}`);
 
-      const coords = [latitude, longitude];
-      // we must have a div element in our HTML with the id name of ('map') so that our map can display in that element
-      map = L.map('map').setView(coords, 12);
-      // console.log(map)
+    console.log(position);
+    // Getting the longitude and latitude
+    // const latitude = position.coords.latitude
+    // const longitude = position.coords.longitude
+    const { longitude } = position.coords;
+    const { latitude } = position.coords;
+    console.log(latitude, longitude);
+    // Getting your current location on google map
+    // console.log(`https://www.google.pt/maps/@${latitude}, ${longitude}`);
 
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+    const coords = [latitude, longitude];
+    // we must have a div element in our HTML with the id name of ('map') so that our map can display in that element
+    this.#map = L.map('map').setView(coords, 12);
+    // console.log(map)
 
-      //Adding an event listener on the map, the mapEvent displays a marker on wherever we click
-      map.on('click', function (mapE) {
-        mapEvent = mapE;
-        form.classList.remove('hidden');
-        inputDistance.focus();
-      });
-    },
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
 
-    // Error callback
-    function () {
-      alert('Could not get current position');
-    }
-  );
+    //Adding an event listener on the map, the mapEvent displays a marker on wherever we click
+    this.#map.on('click', this._showForm.bind(this));
+  }
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
 
-  // Clear input fields
-  inputDistance.value =
-    inputDuration.value =
-    inputCadence.value =
-    inputElevation.value =
-      '';
-  // Display the marker
-  // console.log(mapEvent);
-  const latitude = mapEvent.latlng.lat;
-  const longitude = mapEvent.latlng.lng;
-  // OR
-  // const { lat, lng } = mapEvent.latlng;
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+  _newWorkout(e) {
+    e.preventDefault();
 
-  L.marker([latitude, longitude])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250, // Sets maxwidth of the marker
-        minWidth: 150, // sets minwidth of the marker
-        autoClose: false, // prevents the marker from closing itself
-        closeOnClick: false, // prevents the marker from closing when another position is being clicked
-        className: 'running-popup', // adds a custom class styling to the marker
-      })
-    )
-    .setPopupContent('Workout') // sets the content of the marker
-    .openPopup();
-});
+    // Clear input fields
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+    // Display the marker
+    // console.log(mapEvent);
+    const latitude = this.#mapEvent.latlng.lat;
+    const longitude = this.#mapEvent.latlng.lng;
+    // OR
+    // const { lat, lng } = mapEvent.latlng;
 
-// Toggles the form 'select' type
-inputType.addEventListener('change', function () {
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-});
+    L.marker([latitude, longitude])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250, // Sets maxwidth of the marker
+          minWidth: 150, // sets minwidth of the marker
+          autoClose: false, // prevents the marker from closing itself
+          closeOnClick: false, // prevents the marker from closing when another position is being clicked
+          className: 'running-popup', // adds a custom class styling to the marker
+        })
+      )
+      .setPopupContent('Workout') // sets the content of the marker
+      .openPopup();
+  }
+}
+
+const app = new App();
+// Using the geolocation API (sucess fn, error fn)
