15bd859e0e99c5f72b8d5d3147aaca8d6344cabb
Isaac Ayomide <okunlolaa3@outlook.com>
2024-07-31 21:11:17 +0100

    little fix

--- Asynchronous/script.js
+++ Asynchronous/script.js
@@ -4,40 +4,55 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-const getCountryData = function (country) {
+const renderCountry = function (data) {
+  const countryFlag = data.flags.png;
+  const countryName = data.name.common;
+  const countryRegion = data.region;
+  const countryPopulation = (+data.population / 1000000).toFixed(1);
+  const countryLanguage = Object.values(data.languages);
+  // Object.keys() is a method that returns an array of the keys of the object passed to it. In this case, it returns an array of the keys of the data.currencies object
+  const countryCurrency = data.currencies[Object.keys(data.currencies)].name;
+
+  const html = `
+        <article class="country">
+            <img class="country__img" src="${countryFlag}" />
+            <div class="country__data">
+                <h3 class="country__name">${countryName}</h3>
+                <h4 class="country__region">${countryRegion}</h4>
+                <p class="country__row"><span>👫</span>${countryPopulation} people</p>
+                <p class="country__row"><span>🗣️</span>${countryLanguage}</p>
+                <p class="country__row"><span>💰</span>${countryCurrency}</p>
+            </div>
+        </article>
+`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
   request.send();
 
   request.addEventListener('load', function () {
-    //   console.log(this.responseText); // the endpoint response, but it's in a string format, and it needs to be converted into an object
-
     const [data] = JSON.parse(this.responseText); // converts the stringed object back to string
     console.log(data);
 
-    // console.log(data.languages)
+    // Render country 1
+    renderCountry(data);
 
-    const html = `
-    <article class="country">
-        <img class="country__img" src="${data.flags.png}" />
-        <div class="country__data">
-            <h3 class="country__name">${data.name.common}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(
-              +data.population / 1000000
-            ).toFixed(1)} people</p> 
-            <p class="country__row"><span>🗣️</span>${data.languages.por}</p>
-            <p class="country__row"><span>💰</span>${
-              data.currencies.EUR.name
-            }</p>
-        </div>
-    </article>  
-  `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+    // Get neighbour country
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+
+// AJAX call country 2
+    const request2 = new XMLHttpRequest();
+  request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+  request2.send();
   });
-};
+}; 
 
-getCountryData('portugal');
-// getCountryData('nigeria');
-
+getCountryAndNeighbour('portugal');
