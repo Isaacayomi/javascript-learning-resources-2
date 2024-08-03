d54a11ed7871139636def2aa59474cadea4996a7
Isaac Ayomide <okunlolaa3@outlook.com>
2024-08-03 21:42:06 +0100

    chaining promises

--- Asynchronous/script.js
+++ Asynchronous/script.js
@@ -85,6 +85,17 @@ console.log(request);
 const getCountryData = function (country) {
   const request = fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data[0], 'neighbour'));
 };
-getCountryData('nigeria');
+
+getCountryData('germany');
