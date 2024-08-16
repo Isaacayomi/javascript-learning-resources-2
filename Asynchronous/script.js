45ab030004cddcc17b735078a371f913aaa0996e
Isaac Ayomide <okunlolaa3@outlook.com>
2024-08-16 02:01:03 +0100

    created a simple promise

--- Asynchronous/script.js
+++ Asynchronous/script.js
@@ -199,3 +199,44 @@ const getPosition = function () {
 };
 
 getPosition().then(pos => console.log(pos));
+
+// Coding challenge 2
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    let newImage = document.createElement('img');
+    newImage.src = imgPath;
+
+    newImage.addEventListener('load', function () {
+      imgContainer.append(newImage);
+      resolve(newImage);
+    });
+
+    newImage.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+let currentImg;
+
+createImage('img/img-1.jpg')
+  .then(res => {
+    currentImg = res;
+    console.log('Image 1 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('img/img-2.jpg');
+  })
+  .then(res => {
+    currentImg = res;
+    console.log('Image 2 loaded');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+  })
+  .catch(err => console.error(err));
