12fdbf91abe2cccce4bac4d4e27a767a80ca0f3b
Isaac Ayomide <okunlolaa3@outlook.com>
2024-08-24 21:17:38 +0100

    exporting multiple values using *

--- Modern Js Modules Tooling/modern js starter file/script.js
+++ Modern Js Modules Tooling/modern js starter file/script.js
@@ -1,3 +1,18 @@
 // Importing module
-import './shoppingCart.js';
+// import {
+//   addToCart,
+//   totalQuantity,
+//   totalPrice,
+//   nameNAge,
+// } from './shoppingCart.js';
+// addToCart('bread', 5);
+// nameNAge('Joke', 23);
+
+// console.log(totalPrice, totalQuantity);
+// console.log(addToCart('Beans', 10))
 console.log('Importing module');
+
+// importing all the export of a module at the same time
+import * as ShoppingCart from './shoppingCart.js';
+ShoppingCart.addToCart('bread', 5);
+ShoppingCart.nameNAge('Prime', 50);
--- Modern Js Modules Tooling/modern js starter file/shoppingCart.js
+++ Modern Js Modules Tooling/modern js starter file/shoppingCart.js
@@ -1,2 +1,20 @@
 // Exporting module
-console.log('Exporting module')
+console.log('Exporting module');
+
+const shippingCost = 10;
+const cart = [];
+
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(cart);
+  console.log(`${quantity} ${product} added to cart`);
+};
+
+export const nameNAge = function (name, age) {
+  console.log(`Name is ${name}, and you are ${age} years old`);
+};
+
+const totalPrice = 231;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity };
